5735b6761c6040998182beee25cde4dbead35f39
yash.dhuliya <yash.dhuliya@cybercom.in>
2024-01-23 14:28:47 +0530

    extra pattern

--- Learning/Extra/pattern.php
+++ Learning/Extra/pattern.php
@@ -11,18 +11,38 @@
         $row = $col;
         $mid = (int)($col/2) + 1;
     }
+    
+    {
+        //Brute force - O(3n^2)
+        // for($i=1; $i<=$row; $i++){
+        //     for($j=1; $j<$start; $j++){
+        //         echo "_ ";
+        //     }
+        //     for($k=$start; $k<=$end; $k++){
+        //         echo $k . " ";
+        //     }
+        //     for($j=1; $j<$start; $j++){
+        //         echo "_ ";
+        //     }
+        //     if($i < $mid){
+        //         $start++;
+        //         $end--;
+        //     }else{
+        //         $start--;
+        //         $end++;
+        //     }
+        //     echo "<br>";
+        // }
+    }
 
+    //Optimal - O(n^2)
     for($i=1; $i<=$row; $i++){
-        for($j=1; $j<$start; $j++){
-            echo "_ ";
+        for($j=1; $j<=$col; $j++){
+            if($j>=$start && $j<=$end)
+                echo $j . " ";
+            else    
+                echo "_ ";    
         }
-        for($k=$start; $k<=$end; $k++){
-            echo $k . " ";
-        }
-        for($j=1; $j<$start; $j++){
-            echo "_ ";
-        }
-
         if($i < $mid){
             $start++;
             $end--;

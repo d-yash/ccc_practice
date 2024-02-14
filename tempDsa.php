8b9a63ba5990a68f635e577121818b6fffc5b8ec
yash.dhuliya <y.dhuliya.official@gmail.com>
2024-02-14 09:02:18 +0530

    dsa que

---
+++ tempDsa.php
@@ -0,0 +1,16 @@
+<?php
+ $n = 36;
+$arr = array();
+ for ($i = 1; $i <= sqrt($n); $i++) {
+    if($n % $i == 0){
+        $arr[] = $i;
+        $rem = $n / $i;
+        if(!in_array($rem, $arr)){
+            $arr[] = $rem;
+        }
+    }
+ }
+
+ sort($arr);
+ echo "<pre>";
+ print_r($arr);

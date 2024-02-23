84e7bf6512e2884d7d6648eeecfe57fdd14bb569
yash.dhuliya <y.dhuliya.official@gmail.com>
2024-02-23 18:19:56 +0530

    Filter completed

--- Mvc/app/code/local/Core/Model/Abstract.php
+++ Mvc/app/code/local/Core/Model/Abstract.php
@@ -37,7 +37,10 @@ class Core_Model_Abstract
     }
     public function getCollection()
     {
-        return new $this->_collectionClass();
+        $collection = new $this->_collectionClass();
+        $collection->setResource($this->getResource());
+        $collection->select();
+        return $collection;
     }
     public function getTableName()
     {
--- Mvc/app/code/local/Core/Model/Db/Adapter.php
+++ Mvc/app/code/local/Core/Model/Db/Adapter.php
@@ -23,6 +23,12 @@ class Core_Model_Db_Adapter
     }
     public function fetchAll($query)
     {
+        $row=[];
+        $result = $this->connect()->query($query);
+        while($_row = mysqli_fetch_assoc($result)){
+            $row[] = $_row;
+        }
+        return $row;
     }
     public function fetchPairs($query)
     {
